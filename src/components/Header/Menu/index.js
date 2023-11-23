86df72a7f1319e7016a4caf9e6b99ce4e57dc905
Mohamed <mahameds19999@gmail.com>
2023-11-23 21:28:30 +0200

    Finish header style

--- src/components/Header/Menu/index.js
+++ src/components/Header/Menu/index.js
@@ -1,10 +1,41 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import { useTheme } from "@emotion/react";
+import { IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Menu() {
+  const theme = useTheme();
   return (
     <section>
-      <button>Sign in</button>
-      {/* menu icon */}
+      <Button
+        style={{
+          color: "white",
+          textTransform: "inherit",
+          fontFamily: "inherit",
+          fontSize: `${theme.typography.fontSize.sm}px`,
+          fontWeight: "700",
+          display: "inline-block",
+          marginRight: "1.5rem",
+        }}
+        variant="text"
+      >
+        Sign in
+      </Button>
+      <IconButton
+        style={{
+          backgroundColor: theme.palette.secondary.main,
+        }}
+        aria-label="menu"
+      >
+        <MenuIcon
+          size="large"
+          style={{
+            fontSize: `${theme.typography.fontSize.lg}px`,
+            color: "white",
+          }}
+        />
+      </IconButton>
     </section>
   );
 }
